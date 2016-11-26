5cde1bc28b3a4be12aa662a70ef16263d1d97997
Ian Foster <foster@login4.beagle.ci.uchicago.edu>
2016-11-26 12:59:11 -0600

    add comments

--- run-sweep.swift
+++ run-sweep.swift
@@ -8,10 +8,19 @@ float results[string];
 settingsFilename = argv("settings");
 
 // Obtain list of parameter combinations
+// Settings file should have form:
+//   { 'parameters' : {'1' : numList1, '2': numList2, ...} }
+// where each numList is a list of integers or floats.
+//
+// determineParameters then returns a string with the cross-product of all combinations of numbers from each list.
+// E.g., if there are three parameters:
+//    'n1a,n2a,n3a:n1a,n2b,n3a:...'
+//
 parametersString = python("import sweep", sprintf("sweep.determineParameters(\"%s\")", settingsFilename));
+// We split that string to get a list of comma-separated parameter combinations
 parameters = split(parametersString, ":");
 
-// Run experiments in parallel
+// Run experiments in parallel, passing each a different parameter set
 foreach param in parameters
 {
     resultString = python("import sweep", sprintf("repr(sweep.evaluateOne(%s))", param));
